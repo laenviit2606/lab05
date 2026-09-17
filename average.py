602a5bc1ac4a700dadc302f632c4094501217f79
Levi Duong <longcan2606@gmail.com>
2026-09-17 14:50:41 +0800

    Add average function from chat

---
+++ average.py
@@ -0,0 +1,8 @@
+def average(numbers):
+    if not numbers:           # If the list is empty
+        return 0              # Return 0 instead of crashing
+    total = sum(numbers)      # Add all the numbers together
+    count = len(numbers)      # Count how many numbers there are
+    return total / count      # Divide the total by the count
+print(average([4.50, 12.00, 3.25, 8.75]))
+print(average([]))
